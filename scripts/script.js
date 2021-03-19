24f17c17d6cfb87293056f040058e4edde0a82cb
ltanksley10 <contactlthere@gmail.com>
2021-03-19 18:33:46 +0000

    added testimonial slider

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+function initSwiperCarousel() {
+    if ($(".testimonials-carousel").length > 0) {
+        const swiper = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: '.tc-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: '.listing-carousel-button-next',
+                prevEl: '.listing-carousel-button-prev',
+            },
+            breakpoints: {
+                900: {
+                    slidesPerView: 3,
+                },
+                
+            }
+        });
+    }
+    
+    //  setInterval(function () {
+    //     var size = randomValue(sArray);
+    //     $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+    //     $('.individual-bubble').animate({
+    //         'bottom': '100%',
+    //         'opacity': '-=0.7'
+    //     }, 4000, function () {
+    //         $(this).remove()
+    //     });
+    // }, 350);
+    
+}
+
+$(document).ready(function () {
+    initSwiperCarousel();
+});
